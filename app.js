a2a2cb42baec57ab46c2cc2667de4bb2490a797c
ChristianSheherlis <57498743+ChristianSheherlis@users.noreply.github.com>
2020-09-20 18:21:37 -0400

    cycle 0 notes

---
+++ app.js
@@ -0,0 +1,25 @@
+/*var life = 100; //This is my life bar NUMBER
+
+life = life - 10;
+
+//90
+
+life = life / 10;
+
+//9
+
+var name = "Jeff"; //This is the ign STRING
+
+var checkout = true; //BOOLEAN
+
+var box; //undefined
+*/
+
+//const life = 100 //constant, unchangable
+
+let life = 100; //changeable
+
+life = life - 50;
+
+
+console.log(life);
