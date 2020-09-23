7380d108e7d14131a1c838f57eced0a349e8cc9d
ChristianSheherlis <57498743+ChristianSheherlis@users.noreply.github.com>
2020-09-23 13:50:34 -0400

    homework number 2

--- app.js
+++ app.js
@@ -1,25 +1,89 @@
-/*var life = 100; //This is my life bar NUMBER
+//Functions
 
-life = life - 10;
+/*const name = "Specialist"
 
-//90
+function toUpper(text) {
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+}
 
-life = life / 10;
+toUpper(name); */
 
-//9
+/*const toUpper = () => {
 
-var name = "Jeff"; //This is the ign STRING
+} */
 
-var checkout = true; //BOOLEAN
+//String Concatenation
 
-var box; //undefined
-*/
+/*const name = "Christian";
+const age = 17;
 
-//const life = 100 //constant, unchangable
+const combined = name + age;
 
-let life = 100; //changeable
+console.log(combined);*/
 
-life = life - 50;
+//console.log(typeof combined); visualize datatype
+
+//console.log(`Hello it's me, ${name}`);
+
+//Math
+
+/*const num1 = 100;
+const num2 = 50;
+let val;
+
+//Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4); //always round up
+val = Math.floor(2.8); //always round down
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2); //base first then exponent
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2); //returns the smallest value
+val = Math.max(2, 33, 4, 1, 55, 6, 3, -2); //returns the largest value
+val = Math.random(); //gives a random decimal value less than 1
+val = Math.random() * 20; //multiply that random decimal value by 20
+
+val = Math.floor(Math.random() * 20 + 1); //gives a random whole number between 1 and 20
 
 
-console.log(life);
+console.log(val); */
+
+//Functions-Parameters and Arguments
+
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15){
+    console.log("Running Calculate Bill!");
+    const total = billAmount + (billAmount * taxRate) + (billAmount * tipRate);
+    return total;
+}
+//const wesTotal = 500;
+//const wesTaxRate = 0.3;
+//const myTotal = calculateBill(wesTotal, wesTaxRate);
+const myTotal3 = calculateBill(20 + 20 + 30 + 20, 0.15);
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);
+
+/*function sayHiTo(firstName) {
+    return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo("Specialist");
+console.log(greeting);*/
+
+function doctorize(name) {
+    return `Dr. ${name}`;
+}
+
+function yell(name = "Silly Goose"){
+    return `HEY ${name.toUpperCase()}`;
+}
+
