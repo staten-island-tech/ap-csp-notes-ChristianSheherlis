3bfe7dccf4e5dbb5e21f6a3bec7c7c5a78e58512
ChristianSheherlis <57498743+ChristianSheherlis@users.noreply.github.com>
2020-09-30 10:56:50 -0400

    homework #3

--- app.js
+++ app.js
@@ -1,89 +1,138 @@
-//Functions
+//else if
 
-/*const name = "Specialist"
+/*const dice1 = 6;
+const dice2 = 3;
 
-function toUpper(text) {
-    const upperCased = text.toUpperCase();
-    console.log(upperCased);
+if(dice1 === 6 && dice2 === 6){
+    console.log("You rolled a double");
+} else{
+    console.log("Stay in jail");
+}*/
+
+//objects
+
+/*const user = {
+    name: "Edwin",
+    age: 24,
+    married: false,
+    purchases: ["phone", "car", "laptop"],
+
+    sayName: function() {
+        console.log(this.name);
+    }
+};
+
+user.sayName();
+
+function saymyAge() {
+    console.log(`My age is ${this}`);
 }
 
-toUpper(name); */
+saymyAge();*/
 
-/*const toUpper = () => {
+//switches
 
-} */
+/*const color = 'yellow',
 
-//String Concatenation
+switch(color){
+    case 'red':
+        console.log('Color is red');
+        break;
+    case 'blue':
+        console.log('Color is blue');
+        break;
+    default:
+        console.log('Color is not red or blue');
+        break;
+}
 
-/*const name = "Christian";
-const age = 17;
+let day;
 
-const combined = name + age;
+switch(new Date().getDay()){
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+}
 
-console.log(combined);*/
+console.log(`Today is ${day}`);*/
 
-//console.log(typeof combined); visualize datatype
+//Traversy Arrays
 
-//console.log(`Hello it's me, ${name}`);
+//Create some arrays
+/*const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22,45,33,76,54);
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
 
-//Math
-
-/*const num1 = 100;
-const num2 = 50;
 let val;
 
-//Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+//Get array length
+val = numbers.length;
+//Check if is array
+val = Array.isArray(numbers);
+//Get single value
+val = numbers[3];
+val = numbers[0];
+//Insert into array
+numbers[2] = 100;
+//Find index of value
+val = numbers.indexOf(36);
 
-// Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4); //always round up
-val = Math.floor(2.8); //always round down
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2); //base first then exponent
-val = Math.min(2, 33, 4, 1, 55, 6, 3, -2); //returns the smallest value
-val = Math.max(2, 33, 4, 1, 55, 6, 3, -2); //returns the largest value
-val = Math.random(); //gives a random decimal value less than 1
-val = Math.random() * 20; //multiply that random decimal value by 20
+//mutating arrays
+//add on to the end of array
+numbers.push(250);
+//add on to front
+numbers.unshift(120);
+//Take off from end
+numbers.pop();
+//Take off from front
+numbers.shift();
+//Splice values
+numbers.splice(1, 3);
+//Reverse
+numbers.reverse();
 
-val = Math.floor(Math.random() * 20 + 1); //gives a random whole number between 1 and 20
+//Concatenate array
+val = numbers.concat(numbers2);
 
+//Sorting arrays
+val = fruit.sort();
+val = numbers.sort();
 
-console.log(val); */
+//Use the "compare function"
+val = numbers.sort(function(x, y){
+    return x - y;
+});
 
-//Functions-Parameters and Arguments
+//Reverse sort
+val = numbers.sort(function(x, y){
+    return y - x;
+});
 
-function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15){
-    console.log("Running Calculate Bill!");
-    const total = billAmount + (billAmount * taxRate) + (billAmount * tipRate);
-    return total;
-}
-//const wesTotal = 500;
-//const wesTaxRate = 0.3;
-//const myTotal = calculateBill(wesTotal, wesTaxRate);
-const myTotal3 = calculateBill(20 + 20 + 30 + 20, 0.15);
-const myBill4 = calculateBill(100, undefined, 0.2);
-console.log(myBill4);
-
-/*function sayHiTo(firstName) {
-    return `Hello ${firstName}`;
-}
-
-const greeting = sayHiTo("Specialist");
-console.log(greeting);*/
-
-function doctorize(name) {
-    return `Dr. ${name}`;
+//Find
+function over50(num){
+    return num > 50;
 }
 
-function yell(name = "Silly Goose"){
-    return `HEY ${name.toUpperCase()}`;
-}
+val = numbers.find(over50);
 
+console.log(numbers);
+console.log(val);*/
